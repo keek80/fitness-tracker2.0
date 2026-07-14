3f087e3e326130156a1e154dab7b7183a1b80687
keek80 <62186321+keek80@users.noreply.github.com>
2026-07-14 11:16:04 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -199,7 +199,6 @@ _supabase?.auth?.onAuthStateChange?.((event, session) => {
         console.log('🔑 Password recovery mode activated');
         navigate('update-password');
     }
-    
     if (event === 'SIGNED_OUT') {
         Storage.clearAll();
         showAuthPageView();
